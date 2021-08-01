e9ee697f69934638be7d40408eeb2dc52cec511b
RHRumon <85177445+RHRumon@users.noreply.github.com>
2021-08-01 21:15:42 +0600

    Add files via upload

---
+++ Gregor and Cryptography.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define ll long long
+int main()
+{
+    ll t;
+    scanf("%lld",&t);
+    while(t--)
+    {
+        ll p,i;
+        scanf("%lld",&p);
+        printf("2 %lld\n",p-1);
+
+    }
+    return 0;
+}
